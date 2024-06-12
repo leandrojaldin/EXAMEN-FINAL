baca7e4a26e95fd0cef743209a5abd7d24bbbf19
leandrojaldin <160991852+leandrojaldin@users.noreply.github.com>
2024-06-12 17:16:30 -0400

    enviando cambios en menu de funcion imprimir

--- commits.cpp
+++ commits.cpp
@@ -144,8 +144,7 @@ int main(){
             agregarEvento(identificadorUnico, fecha, evento);
             identificadorUnico += 1;
         }else if(){
-        }else if(comando == "Print"){
-            imprimir();
+        }
         }
 
 
