9c67222a31ad50babed4f98c7d7d3a8ef1955b0a
leandrojaldin <160991852+leandrojaldin@users.noreply.github.com>
2024-06-16 21:13:17 -0400

    Add files via upload
    event_manager.h es donde almacenas las liberiras los nombre de las funciones el map

---
+++ event_manager.h
@@ -0,0 +1,23 @@
+#ifndef EVENT_MANAGER_H
+#define EVENT_MANAGER_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+map<int, vector<string>>mapa1;
+
+void pausar();
+void agregarEvento(int id, string fecha, string nombreEvento);
+void eliminarEvento(string fecha, string evento);
+void buscarEventoConFecha(string fecha);
+void imprimir();
+
+string obtenerPrimeraPalabra(string str);
+string eliminarPrimeraPalabra(string cadena);
+string obtenerLafecha(string cadena);
+
+#endif // EVENT_MANAGER
